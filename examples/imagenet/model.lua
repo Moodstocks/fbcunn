e3ca14d945864fdf2afc7f3e74fe4b5533066732
Mikael Rousson <mikael@moodstocks.com>
2015-01-16 14:17:34 +0100

    avoid unnecessary model creation in retrain mode

--- examples/imagenet/model.lua
+++ examples/imagenet/model.lua
@@ -13,17 +13,25 @@ require 'fbcunn'
 require 'optim'
 
 --[[
-   1. Create Model
+   1. Create or Load Model
    2. Create Criterion
-   3. If preloading option is set, preload weights from existing models appropriately
-   4. Convert model to CUDA
+   3. Convert model to CUDA
 ]]--
 
--- 1.1. Create Network
-local config = opt.netType .. '_' .. opt.backend
-paths.dofile('models/' .. config .. '.lua')
-print('=> Creating model from file: models/' .. config .. '.lua')
-model = createModel(opt.nGPU) -- for the model creation code, check the models/ folder
+-- 1. Create or Load Model
+model = nil
+if opt.retrain ~= 'none' then
+  -- If preloading option is set, preload weights from existing models appropriately
+  assert(paths.filep(opt.retrain), 'File not found: ' .. opt.retrain)
+  print('Loading model from file: ' .. opt.retrain);
+  model = torch.load(opt.retrain)
+else
+  -- Create Network
+  local config = opt.netType .. '_' .. opt.backend
+  paths.dofile('models/' .. config .. '.lua')
+  print('=> Creating model from file: models/' .. config .. '.lua')
+  model = createModel(opt.nGPU) -- for the model creation code, check the models/ folder
+end
 
 -- 2. Create Criterion
 criterion = nn.ClassNLLCriterion()
@@ -34,14 +42,7 @@ print(model)
 print('=> Criterion')
 print(criterion)
 
--- 3. If preloading option is set, preload weights from existing models appropriately
-if opt.retrain ~= 'none' then
-   assert(paths.filep(opt.retrain), 'File not found: ' .. opt.retrain)
-   print('Loading model from file: ' .. opt.retrain);
-   model = torch.load(opt.retrain)
-end
-
--- 4. Convert model to CUDA
+-- 3. Convert model to CUDA
 print('==> Converting model to CUDA')
 model = model:cuda()
 criterion:cuda()

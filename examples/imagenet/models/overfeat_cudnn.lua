dba96874478e466c10f7d29e70cfb9e6dd79af86
Mikael Rousson <mikael@moodstocks.com>
2015-01-22 11:39:20 +0100

    Removed bogus 2-GPU implem. nn.DataParallel() should be used in this case

--- examples/imagenet/models/overfeat_cudnn.lua
+++ examples/imagenet/models/overfeat_cudnn.lua
@@ -1,30 +1,5 @@
-local parallelizeConv = function(c)
-  local mp = nn.ModelParallel(2)
-  local c1 = cudnn.SpatialConvolution(c.nInputPlane, c.nOutputPlane/2, c.kW, c.kH, c.dW, c.dH, c.padW, c.padH)
-  local c2 = cudnn.SpatialConvolution(c.nInputPlane, c.nOutputPlane/2, c.kW, c.kH, c.dW, c.dH, c.padW, c.padH)
-  c1.weight:copy(c.weight:sub(1, c.nOutputPlane/2))
-  c2.weight:copy(c.weight:sub(c.nOutputPlane/2 + 1, -1))
-  c1.bias:copy(c.bias:sub(1, c.nOutputPlane/2))
-  c2.bias:copy(c.bias:sub(c.nOutputPlane/2 + 1, -1))
-  mp:add(c1)
-  mp:add(c2)
-  return mp
-end
-
-local parallelizeNet = function(net)
-   local net2 = nn.Sequential()
-   for _,m in ipairs(net.modules) do
-     if torch.typename(m) == 'cudnn.SpatialConvolution' then
-       net2:add(parallelizeConv(m))
-     else
-       net2:add(m)
-     end
-   end
-   return net2
-end
-
 function createModel(nGPU)
-   assert(nGPU == 1 or nGPU == 2, '1-GPU or 2-GPU  supported for OverFeat')
+   assert(nGPU == 1, 'Only 1-GPU supported for OverFeat')
 
    local features = nn.Sequential()
 
@@ -45,10 +20,6 @@ function createModel(nGPU)
    features:add(cudnn.SpatialConvolution(1024, 1024, 3, 3, 1, 1, 1, 1))
    features:add(cudnn.ReLU(true))
    features:add(cudnn.SpatialMaxPooling(2, 2, 2, 2))
-
-   if nGPU == 2 then
-      features = parallelizeNet(features)
-   end
 
    -- 1.3. Create Classifier (fully connected layers)
    local classifier = nn.Sequential()
